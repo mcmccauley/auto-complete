20fbdf7ae7398ce5bf5bfa2ee4bb401a221a4bcf
Andrew Scott <ascott18@gmail.com>
2016-03-10 20:21:48 -0800

    styling

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 module.exports = {
 	NUM_SUGGESTIONS_TO_RETURN: 10,
 	MINIMUM_ALLOWABLE_FREQUENCY: 5,
-	DATA_PATH: 'out.txt',
-	//DATA_PATH: 'D:/unzipped/out.txt',
+	// DATA_PATH: 'out.txt',
+	DATA_PATH: 'D:/unzipped/out.txt',
 	//REDIS_HOST: "auto-complete-redis.zcdys6.0001.usw2.cache.amazonaws.com",
 	REDIS_HOST: "localhost",
 }
