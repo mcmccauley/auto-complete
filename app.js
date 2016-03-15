6e0430f88b55508062e6abc8dc7a837d9587982b
Andrew Scott <ascott18@gmail.com>
2016-03-15 11:22:08 -0700

    oops. can't do that.

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ client.on("error", function (err) {
 
 
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
